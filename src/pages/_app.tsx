4546d849e53c1e755c3400916a3e5da564f20321
vilsonfcastilho <vilsonfcastilho@gmail.com>
2023-04-20 18:09:30 +0100

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Image from 'next/image';
+import type { AppProps } from 'next/app';
+
+import logoImg from '@/assets/shop-logo.svg';
+
+import { globalStyles } from '@/styles/global';
+import { Container, Header } from '@/styles/pages/app';
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,33 @@
+import { createStitches } from '@stitches/react';
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      white: '#fff',
+      
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e',
+    },
+
+    fontSizes: {
+      s: '1.125rem',
+      m: '1.25rem',
+      l: '1.5rem',
+      xl: '2rem',
+    }
+  },
+});
